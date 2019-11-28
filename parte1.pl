a7f32bd6096fc7bc67b93e1acd7e18b5f0ebb6bc
Felipe Melo <36086450+felipefrm@users.noreply.github.com>
2019-11-28 13:07:42 -0300

    sum

--- parte1.pl
+++ parte1.pl
@@ -1,3 +1,4 @@
+% Insere no final da lista
 inserir_final([], Y, [Y]).         % Se a lista estava vazia, o resultado é [Y]
 inserir_final([I|R], Y, [I|R1]) :- % Senão, o primeiro elemento é igual, e o resto é obtido
     inserir_final(R, Y, R1).       % Inserindo o elemento Y no final do antigo resto
@@ -11,8 +12,24 @@ insereInicio(H, L, [H|L]):- !.
 removeind(0,[_|T],T):- !.
 removeind(I,[H|T],R):- X is I - 1, removeind(X, T, Y), insereInicio(H, Y, R).
 
-%Maior elemento
+% Maior elemento
 max([X,Y|Cauda],Max) :- X >= Y, !, max([X|Cauda],Max).
 max([X,Y|Cauda],Max) :- max([Y|Cauda],Max).
 
-%
+% Interseção de duas linhas
+pertence(X, [X|_]).
+pertence(X, [_|R]) :- pertence(X, R).
+inter([ ], _, [ ]).
+inter([X|L1], L2, [X|L3]):- pertence(X, L2), inter(L1, L2, L3).
+inter([_|L1], L2, L3):- inter(L1, L2, L3).
+
+% Soma dos n primeiros números
+sum(0,0).  		% caso base
+sum(N,R) :-		% se n não for igual a 0: 
+  N > 0,		% considerando n positivo
+  N1 is N-1,	% decrementa o contador
+  sum(N1,R1),	% chama sum para o contador decrementado
+  R is R1+N.	% incrementa o resultado da soma com o contador
+
+
+  
