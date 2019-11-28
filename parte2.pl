3cfbc7debe28e1193e66c4d5ce0146b6b4cc8584
ThalesMrad <39007379+ThalesMrad@users.noreply.github.com>
2019-11-28 10:02:26 -0300

    Update parte2.pl

--- parte2.pl
+++ parte2.pl
@@ -9,9 +9,11 @@ supervisor(luis).
 supervisor_chefe(sonia).
 secretaria(laura).
 diretor(santiago).
-(tecnico(X) , engenheiro(Y)) :- chefe(Y,X).
-(engenheiro(X) , supervisores(Y)) :- chefe(Y,X).
-(analista(X) , supervisor(Y)) :- chefe(Y,X).
-(supervisor(X) , supervisor_chefe(Y)) :- chefe(Y,X).
-(supervisor_chefe(X) , diretor(Y)) :- chefe(Y,X).
-(secretaria(X) , diretor(Y)) :- chefe(Y,X).
+analista().
+chefe(Y,X):-(tecnico(X) , engenheiro(Y)).  
+chefe(Y,X):-(engenheiro(X) , supervisor(Y)).
+chefe(Y,X):-(analista(X) , supervisor(Y)).
+chefe(Y,X):-(supervisor(X) , supervisor_chefe(Y)).
+chefe(Y,X):-(supervisor_chefe(X) , diretor(Y)).
+chefe(Y,X):-(secretaria(X) , diretor(Y)).
+chefe(X,Z):-chefe(X,Y),chefe(Y,Z).
