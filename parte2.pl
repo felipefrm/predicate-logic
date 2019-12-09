4a4ce843411972cf4103479b501ec5ecb8b2b5c8
Felipe Melo <36086450+felipefrm@users.noreply.github.com>
2019-12-09 20:16:28 -0300

    comments

--- parte2.pl
+++ parte2.pl
@@ -1,5 +1,4 @@
 %Atribui a cada pessoa seu cargo
-%cargo(X, analista).
 cargo(rogerio, tecnico).
 cargo(ivone, tecnico).
 cargo(daniel, engenheiro).
@@ -30,13 +29,26 @@ chefiado(supervisor, supervisor_chefe).
 chefiado(supervisor_chefe, diretor).
 chefiado(secretaria, diretor).
 
+% Chamada recursiva para buscar todos os subordinados diretos e 
+% indiretos de um determinado chefe
 chefiados(X,Z):-chefiado(X,Z).
 chefiados(X,Z):-
 	chefiado(X,Y),chefiados(Y,Z).
 
 % Consultas:
-% a) chefiado(tecnico, X) , chefiado(X, Y) 
-% b) chefe(X, ivone) , cargo(X, Y)
-% c) chefiado(X, supervisor_chefe) , cargo(Y, X)  
+
+% % a) chefiado(tecnico, W) , chefiado(W, X), cargo(Y, W) , cargo(Z, X)    
+% 		W = cargo chefe tecnico, X = cargo chefe do chefe do tecnico, 
+% 		Y = nome do chefe do tecnico, Z = nome do chefe do chefe do tencico
+
+% % b) chefe(X, ivone) , cargo(X, Y)
+%		X = nome do chefe da ivone, Y = cargo do chefe da ivone
+%		
+% % c) chefiado(X, supervisor_chefe) , cargo(Y, X)  
 %    chefiado(Z, supervisor) , cargo(W, Z)
-% d) cargo(_,X), not(chefiados(X,diretor)).
+%    	X = cargo do subordinado do supervisor chefe, Y = nome do subordinado do supervisor chefe
+%		Z = cargo do subordinado do supervisor, Y = nome do subordinado do supervisor
+
+% % d) cargo(_,X), not(chefiados(X,diretor)).
+% 		X = cargo da Carolina
+		
